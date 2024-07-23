e9eb161935f85f4126ac0773cc01a7e1d32ed421
Nikita Ryabchikov <nryabchikov24@gmail.com>
2024-07-23 13:22:15 +0300

    feat: project repository crud operations

---
+++ src/main/java/by/digital/chief/tasktracker/repository/ProjectRepository.java
@@ -0,0 +1,9 @@
+package by.digital.chief.tasktracker.repository;
+
+import by.digital.chief.tasktracker.model.Project;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProjectRepository extends JpaRepository<Project, Long> {
+}
